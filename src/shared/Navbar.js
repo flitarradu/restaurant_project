0381df6d9b0c24dbc24cb04e82f6a5ba3af1fd4a
Florina Ceasar <florina.lovin@gmail.com>
2019-03-29 20:36:16 +0200

    Fixed errors

--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -7,40 +7,6 @@ import UserContext from './user.context';
 
 function NavbarCustom (props) {
   return (
-<<<<<<< HEAD
-    <Navbar  bg="dark" variant="dark" >
-      <LinkContainer to="/">
-        <Navbar.Brand > 
-         <img src={logo}  style={{width:60, marginTop: -7}} />
-        </Navbar.Brand>
-      </LinkContainer>
-      <Navbar.Collapse id="basic-navbar-nav" >
-        <Nav className="mr-auto">
-          <LinkContainer exact to="/">
-            <Nav.Link >Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer exact to="/list">
-            <Nav.Link >Book</Nav.Link>
-          </LinkContainer>
-          <LinkContainer exact to="/favorites">
-           <Nav.Link >Favorites</Nav.Link>
-           </LinkContainer> 
-          <LinkContainer exact to="/login">
-            <Nav.Link >Sign in</Nav.Link>
-          </LinkContainer>
-          <LinkContainer exact to="/ContactUs">
-            <Nav.Link >Contact us</Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-      <Form inline>
-      <div className="btn-toolbar">
-         <Button variant="success" className="btn-group mr-2">Log in</Button>
-         <Button variant="outline-primary" >Sign up</Button>
-        </div>
-      </Form>
-    </Navbar>
-=======
     <UserContext.Consumer>
       {
         (function (value) {
@@ -82,7 +48,6 @@ function NavbarCustom (props) {
     })
   }
     </UserContext.Consumer>
->>>>>>> master
   );
 }
 
